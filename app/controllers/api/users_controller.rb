cef9fa5f51467f735bd54e5659122233e2a87ad9
Marcus <mcavini@outlook.com>
2019-03-26 16:46:58 -0400

    Prepare for initial Heroku push

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::UsersController < ApplicationController
+
+    def create  
+        @user = User.new(user_params)
+        if @user.save
+            render :show
+        else
+            render json: @user.errors.full_messages
+        end 
+    end 
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end     
+
+end 
